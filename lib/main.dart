6b3ea0518198b736881979ebdf1d218ba44157fc
Pranika <pranika.shrestha.s21@icp.edu.np>
2023-02-12 20:54:03 +0545

    khalti payment

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
+
 import 'package:eyephoria_pranika_fyp/controller/authentication_controller.dart';
 import 'package:eyephoria_pranika_fyp/controller/cart_controller.dart';
 import 'package:eyephoria_pranika_fyp/controller/category_controller.dart';
-import 'package:eyephoria_pranika_fyp/pages/cart_page.dart';
-import 'package:eyephoria_pranika_fyp/pages/home_page.dart';
-import 'package:eyephoria_pranika_fyp/pages/loader.dart';
-import 'package:eyephoria_pranika_fyp/pages/product_page.dart';
+import 'package:eyephoria_pranika_fyp/payments/khalti.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:khalti/khalti.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Khalti.init(
+    publicKey: 'test_public_key_781201cbfd7644f2923c26f52aaf6564',
+  enabledDebugging: false
+  );
+  Builder: (context, navigatorKey){
+    navigatorKey;
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('ne', 'NP'),
+      ];
+      // LocalizationsDelegate: const [
+      //   KhaltiLocalizations.delegate,
+      // ];
+
+
+      
+  };
+  
+
+  
   Get.put(Authentication());
   Get.put(CategoryController());
   Get.put(CartController());
   runApp(const MaterialApp(
+
       home: GetMaterialApp(
           debugShowCheckedModeBanner: false,
           title: "My First Flutter Project",
-          home: HomePage())));
+          home: KhaltiApp())));
           debugShowCheckedModeBanner: false;
+          
 }
--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,4 +1,4 @@
-const baseUrl = "http://192.168.1.103/eyephoria_api";
+const baseUrl = "http://192.168.1.67/eyephoria_api_database";
 const LOGIN_API = "$baseUrl/login.php";
 const SIGNUP_API = "$baseUrl/singup.php";
 const LOGOUT_API = "$baseUrl/logout.php";
